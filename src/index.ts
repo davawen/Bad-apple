a115f49cb5531ba205c20d4c6b2c7044d3f62c96
davawen <56738724+davawen@users.noreply.github.com>
2021-04-16 19:42:34 +0200

    Removed color, fine tuned greyscale

--- src/index.ts
+++ src/index.ts
@@ -28,42 +28,37 @@ let tokens = [
 //Select video
 let video = process.argv[2] == undefined ? "bad_apple.mp4" : process.argv[2];
 
-function rgbToHex(r: number, g: number, b: number)
-{
-	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+// function hexToRgb(hex: string)
+// {
+// 	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+// 	return result ? {
+// 		r: parseInt(result[1], 16),
+// 		g: parseInt(result[2], 16),
+// 		b: parseInt(result[3], 16)
+// 	} : null;
+// }
 
-function hexToRgb(hex: string)
-{
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
-	} : null;
-}
-
-class Square
-{
-	hex: string;
-	r: number;
-	g: number;
-	b: number;
-	symbol: string;
+// class Square
+// {
+// 	hex: string;
+// 	r: number;
+// 	g: number;
+// 	b: number;
+// 	symbol: string;
 	
-	constructor(hex: string, symbol: string)
-	{
-		this.hex = hex;
+// 	constructor(hex: string, symbol: string)
+// 	{
+// 		this.hex = hex;
 		
-		let _rgb = hexToRgb(hex);
+// 		let _rgb = hexToRgb(hex);
 		
-		this.r  =_rgb.r;
-		this.b = _rgb.b;
-		this.g = _rgb.g;
+// 		this.r  =_rgb.r;
+// 		this.b = _rgb.b;
+// 		this.g = _rgb.g;
 		
-		this.symbol = symbol;
-	}
-}
+// 		this.symbol = symbol;
+// 	}
+// }
 
 async function sleep(time: number)
 {
@@ -76,21 +71,6 @@ async function main()
 	
 	let frames: string[] = [];
 	
-	let squares: Square[] = 
-	[
-			new Square("#DD2E44", "🟥"),
-			new Square('#16C60C', "🟩"),
-			//new Square("#AA8DD7", "🟪"),
-			new Square("#55ACEE", "🟦"),
-			new Square("#C1694F", "🟫"),
-			new Square("#F4900C", "🟧"),
-			new Square("#FBC956", "🟨"),
-			new Square("#E3E5E6", "⬜"),
-			new Square("#31373D", "⬛"),
-			new Square("#DDDDDD", "🔲"),
-			new Square("#808488", "🔳")
-	];
-	
 	const length = fs.readdirSync(`${__dirname}/../ResizedFrames`).length;
 	
 	//Create string from pixels
@@ -98,38 +78,19 @@ async function main()
 	{
 		let pixelData: PixelData = await getPixels(`./ResizedFrames/out-${i + 1}.png`);
 		
-		// let pixels = "";
-		// let pixels = fs.readFileSync(`${__dirname}/FramesCompressed/out-${index+1}.txt`, {encoding: 'ascii'});
-		
 		let mes = "\`";
 		
 		for(let j = 0; j < pixelData.data.length; j += 4)
 		{
-			// console.log(`r${pixelData.data[i]}, g${pixelData.data[i+1]}, b${pixelData.data[i+2]}\n`);
+			let value = .3 * pixelData.data[j] + .59 * pixelData.data[j + 1] + .11 * pixelData.data[j + 2];
 			
-			let value = [pixelData.data[j], pixelData.data[j+1], pixelData.data[j+2]];
-			
-			squares.sort(
-				(a, b) =>
-				{
-					let diffA = (value[0] - a.r)*(value[0] - a.r) + (value[1] - a.g)*(value[1] - a.g) +(value[2] - a.b)*(value[2] - a.b);
-					let diffB = (value[0] - b.r)*(value[0] - b.r) + (value[1] - b.g)*(value[1] - b.g) +(value[2] - b.b)*(value[2] - b.b);
-					
-					return diffA - diffB;
-				}	
-			);
-			
-			mes += squares[0].symbol;
-			
-			// pixels += value;
-			
-			// if(value < 128) mes += "⬛";
-			// else mes += "⬜";
+			if(value < 56) mes += "⬛";
+			else if(value < 131) mes += "🔳";
+			else if(value < 221) mes += "🔲";
+			else mes += "⬜";
 			
 			if(Math.floor(j/4) % 51 == 50) mes += "\n";
 		}
-		
-		// fs.writeFileSync(`${__dirname}/FramesCompressed/out-${index+1}.txt`, pixels, {encoding: 'ascii'});
 		
 		frames.push(mes + "\`");
 		
@@ -165,7 +126,7 @@ async function main()
 						{
 							message.channel.send(frames[Math.floor(frameIndex)]);
 							
-							frameIndex += 6; //Corrects framerate to run at real time
+							frameIndex += 6; //Corrects framerate to run at 30 FPS
 							
 							await sleep(clients.length * 200); //With 10 bots, provide 2000ms of downtime, just barely enough not to lag
 						}
